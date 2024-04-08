39cd16937bcb594b805b7624a395fe58ebc851b6
srijit <srijit@proxtera.com>
2024-04-08 17:32:07 +0530

    ssh test

--- src/features/login/Login.js
+++ src/features/login/Login.js
@@ -30,12 +30,16 @@ const Login = () => {
 
   const handleSubmit = async (values, { setSubmitting }) => {
     try {
-      const res = await axios.post("http://localhost:3000/auth/signin", values, {
-        withCredentials: true,
-        headers: {
-          "CSRF-Token": token,
-        },
-      });
+      const res = await axios.post(
+        "http://localhost:3000/auth/signin",
+        values,
+        {
+          withCredentials: true,
+          headers: {
+            "CSRF-Token": token,
+          },
+        }
+      );
 
       setSubmitting(false);
       alert(res.data.message);
@@ -53,7 +57,7 @@ const Login = () => {
   return (
     <div
       style={{
-        height: "100px",
+        height: "100vh",
         width: "100%",
         display: "flex",
         flexDirection: "column",
@@ -69,17 +73,30 @@ const Login = () => {
       >
         {({ isSubmitting }) => (
           <Form
-            style={{ display: "flex", flexDirection: "column", maxWidth: "500px", width: "90%" }}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              maxWidth: "500px",
+              width: "90%",
+            }}
           >
             <Field
-              style={{ height: "2rem", marginTop: "1rem", padding: "0.25rem 0.5rem" }}
+              style={{
+                height: "2rem",
+                marginTop: "1rem",
+                padding: "0.25rem 0.5rem",
+              }}
               type="email"
               name="email"
               placeholder="Enter your email address"
             />
             <ErrorMessage name="email" component="div" />
             <Field
-              style={{ height: "2rem", marginTop: "1rem", padding: "0.25rem 0.5rem" }}
+              style={{
+                height: "2rem",
+                marginTop: "1rem",
+                padding: "0.25rem 0.5rem",
+              }}
               type="password"
               name="password"
               placeholder="Enter your password"
